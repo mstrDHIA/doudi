9a07622b10a9289dd7d6ba48ab5bef005b5418ea
Mohammed Dhia Elabed <mstrdhia@gmail.com>
2025-01-28 14:25:57 +0100

    click game generalization

--- lib/controllers/click_controller.dart
+++ lib/controllers/click_controller.dart
@@ -9,35 +9,81 @@ class ClickController extends ChangeNotifier{
   int randomNumber = 1;
   late PausableTimer _timer;
   bool showOverlay = false;
+  bool isSolved=false;
   // final AudioPlayer _audioPlayer = AudioPlayer();
 
   void startTimer() {
-    _timer = PausableTimer.periodic(const Duration(seconds: 3), () {
-      randomNumber = Random().nextInt(9) + 1; // Generates a random number between 1 and 9
+    _timer = PausableTimer.periodic(const Duration(seconds: 2), () {
+      int newRandomNumber;
+      do{
+        newRandomNumber = Random().nextInt(9) + 1;
+      }while (newRandomNumber == randomNumber);
+       // Generates a random number between 1 and 9
+       randomNumber = newRandomNumber;
+      // randomNumber = Random().nextInt(9) + 1; // Generates a random number between 1 and 9
       // print(randomNumber);
       notifyListeners();
     });
     _timer.start();
   }
 
-  void showCorrectOverlay() {
+
+    void showResultOverlay(selectedNumber) {
     // print("clicked");
     _timer.pause();
     showOverlay = true;
     
     notifyListeners();
-    _playSound(randomNumber == 1 ? 'assets/audio/correct.mp3' : 'assets/audio/incorrect.mp3');
-    Future.delayed(const Duration(seconds: 1), () {
+    if(randomNumber==selectedNumber){
+      isSolved=true;
+    }
+    if(isSolved){
+      _playSound('assets/audio/correct.mp3');
+      // showOverlay = false;
+    _timer.pause();
+      
+    }
+    else{
+      _playSound('assets/audio/incorrect.mp3');
+       Future.delayed(const Duration(seconds: 1), () {
       showOverlay = false;
-      randomNumber = Random().nextInt(9) + 1; // Generates a random number between 1 and 9
+      int newRandomNumber;
+      do{
+        newRandomNumber = Random().nextInt(9) + 1;
+      }while (newRandomNumber == randomNumber);
+       // Generates a random number between 1 and 9
+       randomNumber = newRandomNumber;
     _timer.reset();
     _timer.start();
-      notifyListeners();
+      
     });
+    }
+    notifyListeners();
+    // _playSound(randomNumber == 1 ? 'assets/audio/correct.mp3' : 'assets/audio/incorrect.mp3');
+   
     
     
     // _timer.start();
   }
+
+  // void showCorrectOverlay() {
+  //   // print("clicked");
+  //   _timer.pause();
+  //   showOverlay = true;
+    
+  //   notifyListeners();
+  //   _playSound(randomNumber == 1 ? 'assets/audio/correct.mp3' : 'assets/audio/incorrect.mp3');
+  //   Future.delayed(const Duration(seconds: 1), () {
+  //     showOverlay = false;
+  //     randomNumber = Random().nextInt(9) + 1; // Generates a random number between 1 and 9
+  //   _timer.reset();
+  //   _timer.start();
+  //     notifyListeners();
+  //   });
+    
+    
+  //   // _timer.start();
+  // }
 
   void _playSound(String soundFile) {
     // Audio.load(soundFile)..play()..dispose();
--- lib/screens/games/click_game/click_screen.dart
+++ lib/screens/games/click_game/click_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:match/controllers/click_controller.dart';
+import 'package:match/controllers/menu_controller.dart';
 import 'package:match/screens/games/click_game/widgets/click_game_widgets.dart';
 import 'package:provider/provider.dart';
 
@@ -12,13 +13,25 @@ class ClickGameScreen extends StatefulWidget{
 
 class _ClickGameScreenState extends State<ClickGameScreen> {
   late ClickController clickController;
+    late MyMenuController menuController;
+
   @override
   void initState() {
     clickController=Provider.of<ClickController>(context, listen: false);
+    menuController=Provider.of<MyMenuController>(context, listen: false);
     // clickController.startTimer();
     // TODO: implement initState
     super.initState();
   }
+  
+  @override
+  void dispose() {
+    clickController.isSolved=false;
+    clickController.showOverlay=false;
+    // TODO: implement dispose
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -47,7 +60,7 @@ class _ClickGameScreenState extends State<ClickGameScreen> {
                     ),
                     InkWell(
                       onTap: () {
-                        clickController.showCorrectOverlay();
+                        clickController.showResultOverlay(menuController.selectedNumber);
                       },
                       child: NumberHolder(clickController: clickController,)),
           
@@ -66,18 +79,21 @@ class _ClickGameScreenState extends State<ClickGameScreen> {
                       )),
               const Positioned(top: 30,right: 30,child: Text("اضغط على الرقم 1",style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold),),),
              
-              if (clickController.showOverlay&&clickController.randomNumber==1)
-                Container(
-                  color: Colors.green.withOpacity(0.5),
-                  child: const Center(
-                    child: Icon(
-                      Icons.check_circle,
-                      color: Colors.white,
-                      size: 100,
+              if (clickController.showOverlay&&clickController.isSolved)
+                GestureDetector(
+                  onTap: () => Navigator.pop(context),
+                  child: Container(
+                    color: Colors.green.withOpacity(0.5),
+                    child: const Center(
+                      child: Icon(
+                        Icons.check_circle,
+                        color: Colors.white,
+                        size: 100,
+                      ),
                     ),
                   ),
                 ),
-              if (clickController.showOverlay&&clickController.randomNumber!=1)
+              if (clickController.showOverlay&&!clickController.isSolved)
                 Container(
                   color: Colors.red.withOpacity(0.5),
                   child: const Center(
